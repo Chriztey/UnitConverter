c3849b4bc3870859ef954d8903f8d9ffb5db915f
Chriztey <CHRISTIETEYDADDY@GMAIL.COM>
2024-03-05 15:12:56 +0700

    rough UI

--- app/src/main/java/com/chris/unitconverter/MainActivity.kt
+++ app/src/main/java/com/chris/unitconverter/MainActivity.kt
@@ -1,18 +1,25 @@
 package com.chris.unitconverter
 
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowDropDown
+import androidx.compose.material3.Button
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
 import com.chris.unitconverter.ui.theme.UnitConverterTheme
 
@@ -39,12 +46,39 @@ class MainActivity : ComponentActivity() {
 fun UnitConverter () {
     Column { // stacked below ea other
         Text(text = "Unit Converter")
-        OutlinedTextField(value = "" , onValueChange = {})
+        OutlinedTextField(value = "Enter Value" , onValueChange = {})
         Row {// stacked beside ea other
+            
+            Box {
+                Button(onClick = { /*TODO*/ }) {
+                    Text(text = "Select")
+                    Icon(Icons.Default.ArrowDropDown,
+                        contentDescription = "Arrow Down")
+                }
+
+            }
+
+            Box {
+                Button(onClick = { /*TODO*/ }) {
+                    Text(text = "Select")
+                    Icon(Icons.Default.ArrowDropDown,
+                        contentDescription = "Arrow Down")
+
+            }
+
+             //Button & Toast Use
+//            val context = LocalContext.current
+//            Button(onClick = { Toast.makeText(context,
+//                "Thanks for Clicking!",
+//                Toast.LENGTH_SHORT).show() }) {
+//                Text(text = "Click Here")
+//
+//            }
 
         }
-        Text(text = "Result")
+
     }
+        Text(text = "Result")
 }
 
 @Composable
@@ -59,4 +93,4 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
 @Composable
 fun UnitConverterPreview(){
     UnitConverter()
-}
+}}
