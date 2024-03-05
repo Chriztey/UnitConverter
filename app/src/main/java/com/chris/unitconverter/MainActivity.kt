3adc54f8df3d46bbe2cd8dfc33eb4667ac23d37c
Chriztey <CHRISTIETEYDADDY@GMAIL.COM>
2024-03-05 16:58:35 +0700

    Update MainActivity.kt
    finish up d5

--- app/src/main/java/com/chris/unitconverter/MainActivity.kt
+++ app/src/main/java/com/chris/unitconverter/MainActivity.kt
@@ -4,13 +4,20 @@ import android.os.Bundle
 import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowDropDown
 import androidx.compose.material3.Button
+import androidx.compose.material3.DropdownMenu
+import androidx.compose.material3.DropdownMenuItem
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
@@ -18,9 +25,11 @@ import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.chris.unitconverter.ui.theme.UnitConverterTheme
 
 class MainActivity : ComponentActivity() {
@@ -44,9 +53,16 @@ class MainActivity : ComponentActivity() {
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun UnitConverter () {
-    Column { // stacked below ea other
+    Column (
+        modifier = Modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally,
+        ) { // stacked below ea other
         Text(text = "Unit Converter")
+        Spacer(modifier = Modifier.height(16.dp))
+        //Spacer(modifier = Modifier.padding(100.dp))
         OutlinedTextField(value = "Enter Value" , onValueChange = {})
+        Spacer(modifier = Modifier.height(16.dp))
         Row {// stacked beside ea other
             
             Box {
@@ -55,8 +71,30 @@ fun UnitConverter () {
                     Icon(Icons.Default.ArrowDropDown,
                         contentDescription = "Arrow Down")
                 }
+                DropdownMenu(expanded = true, onDismissRequest = { /*TODO*/ }) {
+                    DropdownMenuItem(text = {
+                        Text(text = "Centimeters")},
+                        onClick = { /*TODO*/ }
+                    )
+
+                    DropdownMenuItem(text = {
+                        Text(text = "Meters")},
+                        onClick = { /*TODO*/ }
+                    )
+
+                    DropdownMenuItem(text = {
+                        Text(text = "Feet")},
+                        onClick = { /*TODO*/ }
+                    )
+
+                    DropdownMenuItem(text = {
+                        Text(text = "Milimeters")},
+                        onClick = { /*TODO*/ }
+                    )
+                }
 
             }
+            Spacer(modifier = Modifier.width(16.dp))
 
             Box {
                 Button(onClick = { /*TODO*/ }) {
@@ -65,6 +103,27 @@ fun UnitConverter () {
                         contentDescription = "Arrow Down")
 
             }
+                DropdownMenu(expanded = true, onDismissRequest = { /*TODO*/ }) {
+                    DropdownMenuItem(text = {
+                        Text(text = "Centimeters")},
+                        onClick = { /*TODO*/ }
+                    )
+
+                    DropdownMenuItem(text = {
+                        Text(text = "Mters")},
+                        onClick = { /*TODO*/ }
+                    )
+
+                    DropdownMenuItem(text = {
+                        Text(text = "Feet")},
+                        onClick = { /*TODO*/ }
+                    )
+
+                    DropdownMenuItem(text = {
+                        Text(text = "Milimeters")},
+                        onClick = { /*TODO*/ }
+                    )
+                }
 
              //Button & Toast Use
 //            val context = LocalContext.current
@@ -78,6 +137,7 @@ fun UnitConverter () {
         }
 
     }
+        Spacer(modifier = Modifier.height(16.dp))
         Text(text = "Result")
 }
 
